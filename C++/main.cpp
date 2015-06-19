cd10f96f6c69fe86a4552de3e914246a531b490b
penginryo <penginryo@gmail.com>
2015-06-19 11:54:35 -0700

    implement stack and pop function

--- C++/main.cpp
+++ C++/main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 class ErrorFinderForHTML{
 private:
     ifstream input;
+    stack<string> _stack;
 public:
     ErrorFinderForHTML(string filepath){
         input.open(filepath);
@@ -20,18 +21,55 @@ public:
         input.close();
     }
 
-    int openTagFinder(){
+    string tagFinder(){      // li   /li
         char current_char;
         int placeOfOpeningTag;
+        string tagName = "";
 
-        while((current_char = input.get()) != '<'){
-            placeOfOpeningTag = (int) input.tellg();
-            return placeOfOpeningTag;
+        while((current_char = input.get()) != '<') {}
+            //having < at this point
+        while ((current_char = input.get()) != '>') {
+            if (current_char == ' ') break;
+            tagName += current_char;
+        }
+        cout << tagName << endl;
+        return tagName;
+        tagName = "";
+    }
+
+    int stackMan(){
+        string tagName = tagFinder();
+
+        if(tagName[0] == '/') {  // if CLOSING TAG
+            tagName.erase(tagName.begin());
+            if ((_stack.top()) == tagName) {    // check if top of stack is the same as input
+            _stack.pop();
+            }
+        } else {                // if OPENING TAG
+            _stack.push(tagName);
+        }
+
+        cout << "size -> " << _stack.size() << endl << endl;
+
+        if(_stack.size() == 0){
+            return 0;
+        } else {
+            return 1;
         }
     }
-};
 
+};
 int main() {
     ErrorFinderForHTML input("/Users/penginryo/ClionProjects/cpp_stack/test.html");
+
+    int end = -1;
+    end = input.stackMan();
+
+    while(end != 0){
+        end = input.stackMan();
+    }
+
+    cout << "Stack cleared" << endl;
+
     return 0;
 }
